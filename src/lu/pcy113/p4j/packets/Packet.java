19072d0514a8836677234f41855dc613b0780f56
P.Cy.113 <66266372+Poucy113@users.noreply.github.com>
2023-07-05 09:16:25 +0000

    upgraded packets

--- src/lu/pcy113/p4j/packets/Packet.java
+++ src/lu/pcy113/p4j/packets/Packet.java
@@ -3,5 +3,7 @@ package lu.pcy113.p4j.packets;
 public interface Packet {
     
     int id();
+    PacketManager packetManager();
+    String register(PacketManager pm, int id);
     
 }
--- src/lu/pcy113/p4j/packets/s2c/S2CPacket.java
+++ src/lu/pcy113/p4j/packets/s2c/S2CPacket.java
@@ -2,9 +2,7 @@ package lu.pcy113.p4j.packets.s2c;
 
 public interface S2CPacket<T> extends Packet {
 
-    int id();
-
-    T serverWrite(P4JClient client);
+    T serverWrite(ServerClient client);
     void clientRead(ClientServer cserver, T obj);
 
 }
