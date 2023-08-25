960afe5f42cb45ac33c2003ba274f78f5aa5597c
P.Cy.113 <66266372+Poucy113@users.noreply.github.com>
2023-08-25 20:47:03 +0200

    Upgraded build.sh, added fail check, regex selection for compilation files

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,7 @@ bin_dir=""
 version=""
 main_class_name=""
 classp=""
+rgx=""
 
 # Loop through the arguments
 while [ $# -gt 0 ]; do
@@ -20,9 +21,13 @@ while [ $# -gt 0 ]; do
             classp="${1#-cp:}"
             echo "Compiling with classpath: ${classp}"
             ;;
+		-regex:*)
+            rgx="${1#-rgx:}"
+            echo "File Selector set to: ${rgx}"
+            ;;
         *)
             # Unknown argument
-            echo "Unknown argument: $1; use -version:X, -main:X"
+            echo "Unknown argument: $1; use -version:X, -main:X, -cp:X, -regex:X; default regex:*.java"
             exit 1
             ;;
     esac
@@ -30,13 +35,32 @@ while [ $# -gt 0 ]; do
 done
 
 # Check if version is provided
-if [ -z ${version} ]; then
+if [ -z ${version} ];
+then
   version=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' < ../src/lu/pcy113/p4j/version.txt)
   echo "No argument provided; using current version: ${version}"
 else
   #version=$1
   echo "Using provided version: ${version}"
 fi
+
+if [ -z ${rgx} ];
+then
+    rgx="*.java"
+    echo "Using default file selector for compilation: ${rgx}"
+else
+    echo "Using provided file selector for compilation: ${rgx}"
+fi
+
+check {
+    if [ $1 -eq 0 ];
+    then
+        echo $2
+    else
+        echo $3
+        exit 1
+    fi
+}
 
 src_dir="../src/"
 new_version=$(echo "${version}" | tr '.' '_')
@@ -46,31 +70,33 @@ manifest="${out_dir}/MANIFEST.MF"
 
 # Create the bin directory
 mkdir -p "${bin_dir}"
+check $? "Created output binary directory: ${bin_dir}" "Failed to create output binary directory. Aborted."
 
 # Creating and writing the MANIFEST.MF
 echo "Manifest-Version: 1.0" > ${manifest}
+check $? "Created JAR manifest to: ${manifest}" "Failed to create JAR manifest. Aborted."
 
 # Check if main class is provided
-if ! [ -z ${main_class_name} ]; then
+if ! [ -z ${main_class_name} ];
+then
   echo "Main-Class: ${main_class_name}" >> ${manifest}
+  check $? "Added Main Class to JAR manifest: ${main_class_name}" "Failed to add Main Class to JAR manifest. Aborted."
 fi
 
 # Compile sources
 # Find .java files
-find ${src_dir} -name "*.java" > "${out_dir}/sources.txt"
+find ${src_dir} -name ${rgx} > "${out_dir}/sources.txt"
+check $? "Sources collected from ${src_dir} to ${out_dir}" "Couldn't find *.java files in ${src_dir}"
 
 # Compile Java files
-
 if [ -z $classp ]
 then
     javac -nowarn -d "${bin_dir}" "@${out_dir}/sources.txt" 2>&1 | grep -v "^Note:"
 else
     javac -nowarn -d "${bin_dir}" -cp "${classp}" "@${out_dir}/sources.txt" 2>&1 | grep -v "^Note:"
 fi
-
-echo "Compilation done to ${bin_dir}/"
+check $? "Compilation done to ${bin_dir}/" "Compilation failed. Aborted."
 
 # Add to JAR file
 jar cvfm "${out_dir}/packets4j-${version}.jar" "${manifest}" -C "${bin_dir}" . > "/dev/null" 2>&1
-
-echo "JAR file compressed to: ${out_dir}/packets4j-${version}.jar"
+check $? "JAR file compressed to: ${out_dir}/packets4j-${version}.jar" "JAR file compression failed. Aborted."
