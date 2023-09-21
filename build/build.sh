fce7b617ce7a22eee31e3822e4466593c1a9797b
P.Cy.113 <poucy113@school.lu>
2023-09-21 19:09:22 +0200

    Updated build.sh script; added name and package

--- build/build.sh
+++ build/build.sh
@@ -6,6 +6,8 @@ version=""
 main_class_name=""
 classp=""
 rgx=""
+name="packets4j"
+package="lu/pcy113/p4j"
 
 # Loop through the arguments
 while [ $# -gt 0 ]; do
@@ -21,7 +23,7 @@ while [ $# -gt 0 ]; do
             classp="${1#-cp:}"
             echo "Compiling with classpath: ${classp}"
             ;;
-		-regex:*)
+        -regex:*)
             rgx="${1#-rgx:}"
             echo "File Selector set to: ${rgx}"
             ;;
@@ -37,7 +39,7 @@ done
 # Check if version is provided
 if [ -z ${version} ];
 then
-  version=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' < ../src/lu/pcy113/p4j/version.txt)
+  version=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' < ../src/${package}/version.txt)
   echo "No argument provided; using current version: ${version}"
 else
   #version=$1
@@ -46,13 +48,13 @@ fi
 
 if [ -z ${rgx} ];
 then
-    rgx="*.java"
+    rgx="^((?!PrivateMain|txt$).)*$"
     echo "Using default file selector for compilation: ${rgx}"
 else
     echo "Using provided file selector for compilation: ${rgx}"
 fi
 
-check {
+function check {
     if [ $1 -eq 0 ];
     then
         echo $2
@@ -85,18 +87,19 @@ fi
 
 # Compile sources
 # Find .java files
-find ${src_dir} -name ${rgx} > "${out_dir}/sources.txt"
-check $? "Sources collected from ${src_dir} to ${out_dir}" "Couldn't find *.java files in ${src_dir}"
+find ${src_dir} -type f | grep -P ${rgx} > "${out_dir}/sources.txt"
+check $? "Sources collected from ${src_dir} to ${out_dir}" "Couldn't find ${rgx} files in ${src_dir}"
 
 # Compile Java files
 if [ -z $classp ]
 then
-    javac -nowarn -d "${bin_dir}" "@${out_dir}/sources.txt" 2>&1 | grep -v "^Note:"
+    javac -nowarn -d "${bin_dir}" "@${out_dir}/sources.txt" 2>&1
+    check $? "Compilation done to ${bin_dir}/" "Compilation failed. Aborted."
 else
-    javac -nowarn -d "${bin_dir}" -cp "${classp}" "@${out_dir}/sources.txt" 2>&1 | grep -v "^Note:"
+    javac -nowarn -d "${bin_dir}" -cp "${classp}" "@${out_dir}/sources.txt" 2>&1
+    check $? "Compilation done to ${bin_dir}/" "Compilation failed. Aborted."
 fi
-check $? "Compilation done to ${bin_dir}/" "Compilation failed. Aborted."
 
 # Add to JAR file
-jar cvfm "${out_dir}/packets4j-${version}.jar" "${manifest}" -C "${bin_dir}" . > "/dev/null" 2>&1
-check $? "JAR file compressed to: ${out_dir}/packets4j-${version}.jar" "JAR file compression failed. Aborted."
+jar cvfm "${out_dir}/${name}-${version}.jar" "${manifest}" -C "${bin_dir}" . > "/dev/null" 2>&1
+check $? "JAR file compressed to: ${out_dir}/${name}-${version}.jar" "JAR file compression failed. Aborted."
